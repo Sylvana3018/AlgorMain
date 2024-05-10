a95dd7d7bdafd515ba12d92c029d903157b2d4ad
Nyx Store <63561136+Sylvana3018@users.noreply.github.com>
2024-05-10 12:54:56 +0300

    Update Destroy.cs

--- Destroy.cs
+++ Destroy.cs
@@ -7,7 +7,7 @@ public class Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject);
+        Destroy(gameObject); // используем Destroy для уничтожения и gameObject, чтобы выбрать объект к которому привязан скрипт
     }
 
     // Update is called once per frame
